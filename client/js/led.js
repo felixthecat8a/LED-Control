5ee68776f0dfdf0bbdcb74cb3599734982930e56
Felix Ochoa <115331149+felixthecat8a@users.noreply.github.com>
2023-04-29 11:32:33 -0500

    Update led.js

--- client/js/led.js
+++ client/js/led.js
@@ -5,12 +5,10 @@ let isOn = false;
 buttonOnOff.addEventListener("click", handleOnOff);
 function handleOnOff() {
     if (isOn) {
-        //rgb.off();
         isOn = false;
         buttonOnOff.textContent = "Turn On";
         socket.emit('switchOnOff', { isOn:isOn });
     } else {
-        //rgb.on();
         isOn = true;
         buttonOnOff.textContent = "Turn Off";
         socket.emit('switchOnOff', { isOn:isOn });
